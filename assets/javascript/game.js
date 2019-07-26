72ddacaeb5e08f50bc5e09b25260bcd40d1518a2
Tim Sherman <tim.rs.sherman@gmail.com>
2019-07-25 21:11:32 -0700

    one error located at this time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,16 @@ var topics = ["Football", "Basketball", "Soccer", "Gym Fail", "Movies", "Madden"
 
 //functions =====================================================================
 
-        //function displaying the gifs and ratings on the page
+        //function calling for the gifs then displaying the gifs and ratings on the page
 function displayGifList () {
-    var gifSearch = "";
-    //add where to get gifs once function for button rendering made
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var gifSearch = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=aMYkZxZd9DJoNDCwF5t82p0OlpXqgtZ5&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+          console.log(response);
         var newGifDiv = $("<div class = gif>");
 
         for (var i = 0; i < topics.length; i++) {
@@ -30,4 +30,33 @@ function displayGifList () {
         }
       })
 }
+
+        //function for creating the buttons
+function gifButtons () {
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var g = $("<button>");
+        g.addClass("gif-btn");
+        g.attr("data-name", topics[i]);
+        g.text(topics[i]);
+        $("#buttons-view").append(g);
+    }
+}
+
+        //this is for once inputting text to then run the functions (create buttons, display gifs, etc.)
+$("#add-gif").on("click", function(event) {
+    //ERROR....ERROR...stating $ not defined
+    event.preventDefault();
+
+    var gifInput = $("gif-input").val().trim()
+
+    topics.push(gifInput);
+
+    gifButtons();
+})
+
+$(document).on("click", "gif-btn", displayGifList);
+
+gifButtons();
 
